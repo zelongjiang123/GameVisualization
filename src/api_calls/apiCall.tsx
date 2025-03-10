9249794d2d9c3717e11344f1e7ccb9a87cce8730
Zelong Jiang <zjiang287@wisc.edu>
2025-03-10 10:42:01 -0500

    modify the server url

--- src/api_calls/apiCall.tsx
+++ src/api_calls/apiCall.tsx
@@ -16,7 +16,7 @@ interface GetGameResultAPIResponse {
     }[];
 }
 const server_url_local = "http://localhost:8080";
-const server_url = "http://gamesolverbackend-env.eba-mp3wghug.us-east-2.elasticbeanstalk.com";
+const server_url = "https://www.zelongjiang.cc";
 
 export async function getGameResult(rewardMatrix: number[][][], onMessage: (message: string) => void): Promise<GetGameResultResponse> {
 
